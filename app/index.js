d501ba622cf7e42376738c1bc3768f4e0e41c099
Ari Porad <ari@porad.com>
2015-08-03 17:03:15 -0700

    feat(generator): Got prompts working, got basic copying of files from a boilerplate repo

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 'use strict';
 var path = require('path');
 var url = require('url');
+var https = require('https');
+var AdmZip = require('adm-zip');
 var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
@@ -10,10 +12,10 @@ var _ = require('lodash');
 var _s = require('underscore.string');
 
 var proxy = process.env.http_proxy ||
-  process.env.HTTP_PROXY ||
-  process.env.https_proxy ||
-  process.env.HTTPS_PROXY ||
-  null;
+            process.env.HTTP_PROXY ||
+            process.env.https_proxy ||
+            process.env.HTTPS_PROXY ||
+            null;
 
 var githubOptions = {
   version: '3.0.0'
@@ -38,7 +40,58 @@ if (process.env.GITHUB_TOKEN) {
   });
 }
 
-var extractGeneratorName = function (appname) {
+var getRepoZip = function(repo, cb) {
+  // http://stackoverflow.com/questions/10359485/how-to-download-and-unzip-a-zip-file-in-memory-in-nodejs
+  var file_url = 'https://codeload.github.com/' + repo + '/zip/master';
+
+  https.get(file_url, function(res) {
+    var data = [], dataLen = 0;
+
+    res.on('error', console.error);
+    res.on('data', function(chunk) {
+
+      data.push(chunk);
+      dataLen += chunk.length;
+
+    }).on('end', function() {
+      var buf = new Buffer(dataLen);
+
+      for (var i = 0, len = data.length, pos = 0; i < len; i++) {
+        data[i].copy(buf, pos);
+        pos += data[i].length;
+      }
+      cb(buf);
+    });
+  });
+};
+
+function processZippedFilename(filename) {
+  var parts = filename.split('/');
+  parts.shift();
+  var newName = parts.join('/');
+
+  return newName;
+}
+
+// TODO: This would be a really great place to use ES6 yeild.
+function unzip(buffer, cb) {
+  var zip = new AdmZip(buffer);
+  var zipEntries = zip.getEntries();
+
+  var files = {};
+  for (var i = 0; i < zipEntries.length; i++) {
+    var entry = zipEntries[i];
+    var name = processZippedFilename(entry.entryName);
+    var text = zip.readAsText(entry);
+    if (entry.isDirectory) continue;
+    console.log(chalk.cyan('    unzip ') + name);
+    files[name] = text;
+  }
+
+  return files;
+}
+
+var extractGeneratorName = function(appname) {
   var match = appname.match(/^generator-(.+)/);
 
   if (match && match.length === 2) {
@@ -54,10 +107,10 @@ var emptyGithubRes = {
   html_url: ''
 };
 
-var githubUserInfo = function (name, cb, log) {
+var githubUserInfo = function(name, cb, log) {
   github.user.getFrom({
     user: name
-  }, function (err, res) {
+  }, function(err, res) {
     if (err) {
       log.error('Cannot fetch your github profile. Make sure you\'ve typed it correctly.');
       res = emptyGithubRes;
@@ -68,7 +121,7 @@ var githubUserInfo = function (name, cb, log) {
 };
 
 module.exports = generators.Base.extend({
-  constructor: function () {
+  constructor: function() {
     generators.Base.apply(this, arguments);
 
     this.option('flat', {
@@ -79,7 +132,7 @@ module.exports = generators.Base.extend({
     });
   },
 
-  initializing: function () {
+  initializing: function() {
     this.pkg = require('../package.json');
     this.currentYear = (new Date()).getFullYear();
     this.config.set('structure', this.options.flat ? 'flat' : 'nested');
@@ -88,57 +141,89 @@ module.exports = generators.Base.extend({
   },
 
   prompting: {
-    askFor: function () {
+    repo: function() {
       var done = this.async();
 
-      this.log(yosay('Create your own ' + chalk.red('Yeoman') + ' generator with superpowers!'));
+      this.log(yosay('Create your own ' +
+                     chalk.red('Yeoman') +
+                     ' generator from a boilerplate repo!'));
 
-      var prompts = [{
-        name: 'githubUser',
-        message: 'Would you mind telling me your username on GitHub?',
-        default: 'someuser'
-      }];
+      var prompts = [
+        {
+          name: 'githubUser',
+          message: 'Would you mind telling me your username on GitHub?',
+          default: 'someuser'
+        },
+        {
+          name: 'repo',
+          message: 'If you don\'t mind, what repository are you making a ' +
+                   'generator for',
+          default: function(props) {
+            return props.githubUser + '/boilerplate'
+          },
+          validate: function(res) {
+            return !!res.match(/^([A-z\-]{4,}\/[A-z\-]+)$/i) ||
+                   'My apologies, but that doesn\'t appear to be a repository';
+          }
+        }
+      ];
 
-      this.prompt(prompts, function (props) {
-        this.githubUser = props.githubUser;
+      this.prompt(prompts, function(props) {
+        this.props = props;
         done();
       }.bind(this));
     },
 
-    askForGeneratorName: function () {
+    name: function() {
       var done = this.async();
-      var generatorName = extractGeneratorName(this.appname);
 
-      var prompts = [{
-        name: 'generatorName',
-        message: 'What\'s the base name of your generator?',
-        default: generatorName
-      }, {
-        type: 'confirm',
-        name: 'askNameAgain',
-        message: 'The name above already exists on npm, choose another?',
-        default: true,
-        when: function (answers) {
-          var done = this.async();
-          var name = 'generator-' + answers.generatorName;
+      var prompts = [
+        {
+          name: 'name',
+          message: 'If I may, what do you plan to name your ' +
+                   chalk.bold('fabulous') + ' generator?',
+          default: function(props) {
+            return 'generator-' + _s.slugify(this.props.repo.split('/')[1]);
+          }.bind(this),
+          validate: function(res) {
+            return !!res.match(/^((?:generator\-)?[A-z][A-z\-]{0,201}[A-z])$/i) ||
+                   'My apologies, but that doesn\'t appear to be a valid npm ' +
+                   'package name.';
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'askNameAgain',
+          message: 'The name above already exists on npm, choose another?',
+          default: true,
+          when: function(props) {
+            var done = this.async();
+            var name = props.name;
 
-          npmName(name, function (err, available) {
-            if (!available) {
-              done(true);
-            }
+            npmName(name, function(err, available) {
+              if (!available) {
+                done(true);
+              }
 
-            done(false);
-          });
+              done(false);
+            });
+          }
         }
-      }];
+      ];
 
-      this.prompt(prompts, function (props) {
+      this.prompt(prompts, function(props) {
         if (props.askNameAgain) {
-          return this.prompting.askForGeneratorName.call(this);
+          return this.prompting.name.call(this);
         }
 
-        this.generatorName = props.generatorName;
-        this.appname = _s.slugify('generator-' + this.generatorName);
+        if (props.name.indexOf('generator-') == -1) {
+          props.name = 'generator-' + props.name;
+        }
+
+        props.generatorName = extractGeneratorName(props.name);
+        props.name = _s.slugify(props.name);
+
+        this.props = _.extend(this.props, props);
 
         done();
       }.bind(this));
@@ -146,28 +231,51 @@ module.exports = generators.Base.extend({
   },
 
   configuring: {
-    enforceFolderName: function () {
-      if (this.appname !== _.last(this.destinationRoot().split(path.sep))) {
-        this.destinationRoot(this.appname);
+    enforceFolderName: function() {
+      if (this.props.name !==
+          _.last(this.destinationRoot().split(path.sep))) {
+        this.destinationRoot(this.props.name);
       }
 
       this.config.save();
     },
 
-    userInfo: function () {
+    userInfo: function() {
       var done = this.async();
-
-      githubUserInfo(this.githubUser, function (res) {
-        this.realname = res.name;
-        this.email = res.email;
-        this.githubUrl = res.html_url;
+      console.log(this.props);
+      githubUserInfo(this.props.githubUser, function(res) {
+        this.props.realname = res.name;
+        this.props.email = res.email;
+        this.props.githubUrl = res.html_url;
         done();
       }.bind(this), this.log);
     }
   },
 
   writing: {
-    projectfiles: function () {
+    setup: function() {
+      console.log(this.props);
+      for (var key in this.props) {
+        this[key] = this.props[key];
+      }
+    },
+
+    getGitRepo: function() {
+      var done = this.async();
+      getRepoZip(this.props.repo, function(buf) {
+        var files = unzip(buf);
+
+        for (var path in files) {
+          this.fs.write(path, files[path]);
+        }
+
+        this.files = files;
+
+        done();
+      }.bind(this));
+    },
+
+    projectfiles: function() {
       this.template('_package.json', 'package.json');
       this.template('editorconfig', '.editorconfig');
       this.template('jshintrc', '.jshintrc');
@@ -175,12 +283,12 @@ module.exports = generators.Base.extend({
       this.template('README.md');
     },
 
-    gitfiles: function () {
+    gitfiles: function() {
       this.copy('gitattributes', '.gitattributes');
       this.copy('gitignore', '.gitignore');
     },
 
-    app: function () {
+    app: function() {
       this.fs.copyTpl(
         this.templatePath('app/index.js'),
         this.destinationPath(this.generatorsPrefix, 'app/index.js'),
@@ -190,42 +298,20 @@ module.exports = generators.Base.extend({
         }
       );
     },
-
-    templates: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath(this.generatorsPrefix, 'app/templates/editorconfig')
-      );
-
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath(this.generatorsPrefix, 'app/templates/jshintrc')
-      );
-
-      this.fs.copy(
-        this.templatePath('app/templates/_package.json'),
-        this.destinationPath(this.generatorsPrefix, 'app/templates/_package.json')
-      );
-
-      this.fs.copy(
-        this.templatePath('app/templates/_bower.json'),
-        this.destinationPath(this.generatorsPrefix, 'app/templates/_bower.json')
-      );
-    },
-
-    tests: function () {
-      this.fs.copyTpl(
-        this.templatePath('test-app.js'),
-        this.destinationPath('test/test-app.js'),
-        {
-          prefix: this.generatorsPrefix,
-          generatorName: this.generatorName
-        }
-      );
-    }
   },
 
-  install: function () {
+  tests: function() {
+    this.fs.copyTpl(
+      this.templatePath('test-app.js'),
+      this.destinationPath('test/test-app.js'),
+      {
+        prefix: this.generatorsPrefix,
+        generatorName: this.generatorName
+      }
+    );
+  },
+
+  install: function() {
     this.installDependencies({ bower: false });
-  }
+  },
 });
--- app/templates/README.md
+++ app/templates/README.md
@@ -1,4 +1,4 @@
-# <%= appname %> [![Build Status](https://secure.travis-ci.org/<%= githubUser %>/<%= appname %>.png?branch=master)](https://travis-ci.org/<%= githubUser %>/<%= appname %>)
+# <%= name %> [![Build Status](https://secure.travis-ci.org/<%= githubUser %>/<%= name %>.png?branch=master)](https://travis-ci.org/<%= githubUser %>/<%= name %>)
 
 > [Yeoman](http://yeoman.io) generator
 
@@ -23,10 +23,10 @@ npm install -g yo
 
 Yeoman travels light. He didn't pack any generators when he moved in. You can think of a generator like a plug-in. You get to choose what type of application you wish to create, such as a Backbone application or even a Chrome extension.
 
-To install <%= appname %> from npm, run:
+To install <%= name %> from npm, run:
 
 ```bash
-npm install -g <%= appname %>
+npm install -g <%= name %>
 ```
 
 Finally, initiate the generator:
