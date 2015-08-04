408e124dcbdefef76ea3f2af5c5435a0e9b396c7
Ari Porad <ari@porad.com>
2015-08-04 16:47:02 -0700

    feat(package.json): Make package.json be completely customized

--- app/templates/app/index.js
+++ app/templates/app/index.js
@@ -7,14 +7,15 @@ var _s = require('underscore.string');
 var GitHub = require('../../github');
 
 var repo = '<%= repo %>';
+var emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 
-function rewrite(file, name){
+function rewrite(file, name) {
   return file
-            .replace(/({{#\s*=?\s*name\s*#}})/i, name)
+    .replace(/({{#\s*=?\s*name\s*#}})/i, name)
 }
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
+  prompting: function() {
     var done = this.async();
 
     // Have Yeoman greet the user.
@@ -22,13 +23,51 @@ module.exports = yeoman.generators.Base.extend({
       'Welcome to the ' + chalk.cyan('<%= repo %>') + ' generator!'
     ));
 
-    var prompts = [{
-      name: 'name',
-      message: 'If I may ask, what name have you chosen for your new app?',
-      default: 'My Awesome App'
-    }];
+    var prompts = [
+      {
+        name: 'userName',
+        message: 'If I may ask, what is your name',
+        default: 'Octo Cat',
+        validate: function(res) {
+          return !!res.match(/^([A-Za-z]{3,} [A-Za-z]{3,})$/i) ||
+                 'My apologies, but that doesn\'t appear to be a name.';
+        }
+      },
+      {
+        name: 'userEmail',
+        message: 'Please excuse me, but may I please ask your name?',
+        validate: function(res) {
+          return !!res.match(emailRegex) ||
+                 'My apologies, but that doesn\'t apear to be a valid email.';
+        }
+      },
+      {
+        name: 'githubUser',
+        message: 'Would you mind telling me your username on GitHub?',
+        default: 'octocat',
+        validate: function(res) {
+          if (!GitHub.validateUser(res)) {
+            return 'My apologies, but that doesn\'t appear to be a valid GitHub username';
+          }
 
-    this.prompt(prompts, function (props) {
+          var done = this.async();
+          GitHub.checkItemExists(res, function(err, exists) {
+            if (err) {
+              throw err;
+            }
+            done(exists ||
+                 'My apologies, but that doesn\'t appear to be a user on GitHub');
+          });
+        }
+      },
+      {
+        name: 'name',
+        message: 'If I may ask, what name have you chosen for your new app?',
+        default: 'My Awesome App'
+      }
+    ];
+
+    this.prompt(prompts, function(props) {
       // To access props later use this.props.someOption;
       this.props = JSON.parse('<%= JSON.stringify(props) %>');
       for (var key in props) {
@@ -36,6 +75,11 @@ module.exports = yeoman.generators.Base.extend({
           this.props[key] = props[key];
         }
       }
+      this.props.githubUrl = [
+        'https://github.com',
+        this.props.githubUser,
+        _s.slugify(this.props.name)
+      ].join('/');
       done();
     }.bind(this));
   },
@@ -44,11 +88,11 @@ module.exports = yeoman.generators.Base.extend({
     everything: function() {
       var done = this.async();
 
-      GitHub.getRepo(repo, function(err, files){
+      GitHub.getRepo(repo, function(err, files) {
         if (err) throw err;
 
         for (var path in files) {
-          if(!files.hasOwnProperty(path)) continue;
+          if (!files.hasOwnProperty(path)) continue;
           this.fs.write(path, rewrite(files[path], this.props.name));
         }
 
@@ -59,6 +103,14 @@ module.exports = yeoman.generators.Base.extend({
     packageJsonRename: function() {
       var pkg = this.fs.readJSON('package.json');
       pkg.name = _s.slugify(this.props.name);
+      pkg.repository.url = pkg.homepage = this.props.githubUrl;
+      pkg.bugs.url = this.props.githubUrl + '/issues';
+      pkg.author = {
+        name: this.props.userName,
+        "email": this.props.userEmail,
+        "url": this.props.githubUrl
+      };
+
       this.fs.writeJSON('package.json', pkg);
     },
 
